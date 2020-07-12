1b60158a9737ea9fa4eda98b35285dd073123e6a
juztz1n <35197661+juztz1n@users.noreply.github.com>
2020-07-12 23:17:09 +0200

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,43 @@
+// Scroll Dataset for CSS.
+const debounce = (fn) =>
+{
+    let frame;
+
+    return (...params) =>
+    {
+        if (frame)
+        {
+            cancelAnimationFrame(frame);
+        }
+
+        frame = requestAnimationFrame(() =>
+        {
+            fn(...params);
+        });
+	}
+};
+
+const storeScroll = () =>
+{
+    document.documentElement.dataset.scroll = window.scrollY;
+}
+
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+storeScroll();
+
+// Scroll To.
+function goToHeader() {
+    var element = document.getElementById("header");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function goToContent() {
+    var element = document.getElementById("projects");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function goToFooter() {
+    var element = document.getElementById("footer");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
